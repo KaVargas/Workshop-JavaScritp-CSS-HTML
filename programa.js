a6466ae1e164c7c47aa03448219befc826c704fa
Kenedy Vargas <35229437+KaVargas@users.noreply.github.com>
2018-12-25 10:18:48 -0500

    sended code

---
+++ programa.js
@@ -0,0 +1,21 @@
+var persons = 
+[
+    {firstName:"Juan", lastName:"Rodriguez", heigh:70, altura:150},
+    {firstName:"Rodrigo", lastName:"Almache", heigh:100, altura:150},
+    {firstName:"Jose", lastName:"Torres", heigh:80, altura:160},
+    {firstName:"Francisco", lastName:"Arias", heigh:45, altura:160},
+    {firstName:"Luis", lastName:"Ruiz", heigh:78, altura:170},
+    {firstName:"Maria", lastName:"Fernandez", heigh:102, altura:180},
+    {firstName:"Omar", lastName:"Ponce", heigh:56, altura:180},
+    {firstName:"Fernanda", lastName:"Fernandez", heigh:90, altura:140},
+    {firstName:"Bolivar", lastName:"Ochoa", heigh:87, altura:170},
+    {firstName:"Jhon", lastName:"Fernandez", heigh:76, altura:190},
+    {firstName:"Andrea", lastName:"Alvarado", heigh:85, altura:190}
+];
+
+var per1 = {firstName:"Juan", lastName:"Rodriguez", heigh:70, altura:150};
+
+function cambiarnombre(){
+    document.getElementById("test1").innerHTML = "5";
+}
+
